a9a67498e2c1fb409d40eaf1ebfdd7779bc931cd
henrybeattie2 <henry.beattie@stvincent.edu>
2022-10-28 02:59:04 -0400

    added shoot script, bullet script and a bullet sprite

---
+++ GameDevProj1/Assets/Scripts/Bullet.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject Asteroid;
+    public GameObject bullet;
+    public Vector2 direction;
+    public float speed = 8f;
+
+    void Start()
+    {
+        Destroy(this.gameObject, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Move();
+
+    }
+    private void Move()
+    {
+        float halfHeight = 60.5f;
+        float halfWidth = 60.5f;
+        Vector2 currentPosition = Camera.main.WorldToScreenPoint(this.transform.position);
+
+        if ((currentPosition.x + halfWidth) > Screen.width)//right
+        {
+            direction *= new Vector2(-1, 1);
+        }
+        if ((-currentPosition.x + halfWidth) > Screen.width - Screen.width)//left
+        {
+            direction *= new Vector2(-1, 1);
+        }
+        if ((currentPosition.y + halfHeight) > Screen.height)//top
+        {
+            direction *= new Vector2(1, -1);
+        }
+        if ((-currentPosition.y + halfHeight) > Screen.height - Screen.height)//bottom
+        {
+            direction *= new Vector2(1, -1);
+        }
+        Vector3 newPosition = new Vector3(5 * transform.up.x * Time.deltaTime, 5 * transform.up.y * Time.deltaTime, 0);
+        this.transform.position += newPosition;
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject gameObject = collision.gameObject;
+        if (gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+            GameObject g = Instantiate(collectable);
+            g.transform.position = new Vector3(Random.Range(-3, 3), 3, 0);
+        }
+        Destroy(this.gameObject);
+    }
+}
---
+++ GameDevProj1/Assets/Scripts/Shoot.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject Bullet;
+    public GameObject shootPoint;
+    public float coolDownTime = 0.5f;
+
+    private bool inCoolDown = false;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) && !inCoolDown)
+        {
+            inCoolDown = true;
+
+            GameObject go = Instantiate(Bullet);
+            //go.transform.SetPositionAndRotation(shootPoint.transform.position, shootPoint.transform.rotation);
+            go.transform.position = shootPoint.transform.position;
+            go.transform.rotation = shootPoint.transform.rotation;
+            Bullet b = go.GetComponent<Bullet>();
+            b.speed = 10;
+
+            StartCoroutine(CoolDown());
+        }
+    }
+
+    IEnumerator CoolDown()
+    {
+        yield return new WaitForSeconds(coolDownTime);
+        inCoolDown = false;
+    }
+}
+
